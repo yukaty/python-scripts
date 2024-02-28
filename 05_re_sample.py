03ee1876b1fd645128c002ce98cb586b7ce47db1
ytamura <ytamura@example.com>
2024-02-28 14:35:34 -0700

    Initial commit

---
+++ 05_re_sample.py
@@ -0,0 +1,31 @@
+import re
+
+email = 'abc@example.com'
+
+# Match
+match_string = re.match(r'[a-z]+@[a-z]+\.[a-z]+', email)
+print(match_string)
+print(match_string.start())
+print(match_string.end())
+print(match_string.span())
+print(match_string.group())
+
+# Search
+emails = 'aaa@example.com bbb@example.com ccc@example.com'
+match_list = re.findall(r'[a-z]+@[a-z]+\.[a-z]+', emails)
+print(match_list)
+
+# Replace
+replace_string = re.sub(r'[a-z]+@', 'ABC@', email)
+print(replace_string)
+
+# Split
+split_string = re.split(r'@', email)
+print(split_string)
+
+# Text Cleaning 
+# Get only alphabets and spaces (^: not) and convert to lower case
+text = "HELLO, WORLD! This is a sample text with numbers [123] and special characters [#@&]."
+cleaned_text = re.sub(r'[^a-zA-Z\s]', '', text)
+cleaned_text = cleaned_text.lower()
+print(cleaned_text)
